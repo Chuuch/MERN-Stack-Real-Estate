1ee266a2f85966fdc28a3e8403cf593df5080d4e
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 19:04:52 -0800

    Added routing for pages in App comp.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
+import Profile from './pages/Profile/Profile.jsx';
+import SignUp from './pages/SignUp/SignUp.jsx';
+import SignIn from './pages/SignIn/SignIn.jsx';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/signin" element={<SignIn />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default App
+export default App;
