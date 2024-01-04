ae67e772c954028be161129850f6739a3c6be262
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-04 01:17:04 -0800

    Added logo & ico icon

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { TbHomeSearch } from 'react-icons/tb'
 
 const Header = () => {
 	return (
 		<header className="bg-slate-200 shadow-md">
 			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-				<Link to="/">
+				<Link to="/" className='flex flex-row items-center'>
+					{/* <img src='./logo.png' alt='logo' className='w-10'/> */}
+					<TbHomeSearch size={32} className='text-gray-500'/>
 					<h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-						<span className="text-slate-500">Chuchulev</span>
-						<span className="text-slate-700">Estate</span>
+						<span className="text-slate-500">Home</span>
+						<span className="text-slate-700">4</span>
+						<span className='text-gray-400'>You</span>
 					</h1>
 				</Link>
 				<form className="bg-slate-100 p-3 rounded-lg flex items-center">
