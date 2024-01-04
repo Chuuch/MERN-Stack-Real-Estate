72ba0cdee70f415d49a8888b6bec1d3773c7a7c4
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-04 13:36:58 -0800

    added json option for express

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-
 dotenv.config();
 
 mongoose
@@ -16,8 +15,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-	console.log('Server listening on port 3000');
+	console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRouter);
+
