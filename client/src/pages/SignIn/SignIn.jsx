e8fd7982eb778808295a490ceadccb7009d28047
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-05 13:47:35 -0800

    Added OAuth option to Sign-up & Sign-in

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../../redux/user/userSlice.js';
+import OAuth from '../../components/OAuth.jsx';
 
 const SignIn = () => {
 	const [formData, setFormData] = useState({});
@@ -69,6 +70,7 @@ const SignIn = () => {
 				>
 					{loading ? 'Loading...' : 'Sign In'}
 				</button>
+				<OAuth />
 			</form>
 			<div className="flex gap-2 mt-5">
 				<p>Dont have an account?</p>
