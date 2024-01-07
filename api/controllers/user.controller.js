f95f81085895108610c4ac13552ed2b4a6229f85
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-06 16:52:29 -0800

    cleaned user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,7 +32,9 @@ export const updateUser = async (req, res, next) => {
 
     const { password, ...rest } = updatedUser._doc;
 
-    res.status(200).json(rest);
+    res
+    .status(200)
+    .json(rest);
   } catch (error) {
     next(error);
   }
@@ -43,8 +45,10 @@ export const deleteUser = async (req, res, next) => {
     return next(errorHandler(401, 'You can only delete your own account!'));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted!');
+    res
+    .status(200)
+    .json('User has been deleted!')
+    .clearCookie('access_token');
   } catch (error) {
     next(error);
   }
@@ -54,7 +58,9 @@ export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
       const listings = await Listing.find({ userRef: req.params.id });
-      res.status(200).json(listings);
+      res
+      .status(200)
+      .json(listings);
     } catch (error) {
       next(error);
     }
@@ -72,7 +78,9 @@ export const getUser = async (req, res, next) => {
   
     const { password: pass, ...rest } = user._doc;
   
-    res.status(200).json(rest);
+    res
+    .status(200)
+    .json(rest);
   } catch (error) {
     next(error);
   }
