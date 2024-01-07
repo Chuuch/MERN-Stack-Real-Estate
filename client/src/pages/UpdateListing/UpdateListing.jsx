5fa6aa657ce270c2169403e01776244c9189758b
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-07 15:15:02 -0800

    fixed invalid import path for firebase config

--- client/src/pages/UpdateListing/UpdateListing.jsx
+++ client/src/pages/UpdateListing/UpdateListing.jsx
@@ -6,7 +6,7 @@ import {
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
-import { app } from '../firebase';
+import { app } from '../../config/firebase-config.js';
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 
