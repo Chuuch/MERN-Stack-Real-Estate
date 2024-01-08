3105e8599acb8136dca999a3565fa64c944d36fb
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-07 20:05:44 -0800

    checking signing

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -9,14 +9,12 @@ const SignIn = () => {
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-	const handleChange = (e) => {
-		setFormData({
-			...formData,
-			[e.target.id]: e.target.value,
-		});
-	};
-  
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -40,47 +38,42 @@ const SignIn = () => {
       dispatch(signInFailure(error.message));
     }
   };
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-	return (
-		<div className="p-3 max-w-lg mx-auto">
-			<h1 className="text-3xl text-center font-semibold my-7 dark:text-gray-400">Sign in</h1>
-			<form
-				onSubmit={handleSubmit}
-				className="flex flex-col gap-4"
-				type="text"
-			>
-				<input
-					type="email"
-					placeholder="email"
-					className="border p-3 rounded-lg"
-					id="email"
-					onChange={handleChange}
-				/>
-				<input
-					type="password"
-					placeholder="password"
-					className="border p-3 rounded-lg"
-					id="password"
-					onChange={handleChange}
-				/>
-				<button
-					disabled={loading}
-					className="bg-slate-700 text-white p-3 rounded-lg uppercase cursor-pointer
-            hover:opacity-95 disabled:opacity-80"
-				>
-					{loading ? 'Loading...' : 'Sign In'}
-				</button>
-				<OAuth />
-			</form>
-			<div className="flex gap-2 mt-5">
-				<p className='dark:text-gray-600'>Dont have an account?</p>
-				<Link className="hover:underline" to="/sign-up">
-					<span className="text-blue-700">Sign up</span>
-				</Link>
-			</div>
-			{error && <p className='text-red-500 mt-5'>{error}</p>}
-		</div>
-	);
-};
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+        <OAuth/>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
+  );
+}
 
 export default SignIn;
